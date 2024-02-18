e7a61c8eec7aa80bd82e49301c134f3dedd4d51b
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-02-19 01:03:26 +0300

    Add files via upload

---
+++ uygulama 1-1550/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace uygulama_1_1550
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMesajGoster = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMesajGoster
+            // 
+            this.btnMesajGoster.Location = new System.Drawing.Point(132, 44);
+            this.btnMesajGoster.Name = "btnMesajGoster";
+            this.btnMesajGoster.Size = new System.Drawing.Size(357, 87);
+            this.btnMesajGoster.TabIndex = 0;
+            this.btnMesajGoster.Text = "Mesaj Göster ";
+            this.btnMesajGoster.UseVisualStyleBackColor = true;
+            this.btnMesajGoster.Click += new System.EventHandler(this.btnMesajGoster_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 162);
+            this.Controls.Add(this.btnMesajGoster);
+            this.Name = "Form1";
+            this.Text = "form 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMesajGoster;
+    }
+}
+
